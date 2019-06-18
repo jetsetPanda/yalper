f9f319621aedeb71d0411540189b439961bb75a9
Omar <marionong@gmail.com>
2019-06-18 01:11:05 -0400

    now more delightfully heroku ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import BusinessList from '../BusinessList/BusinessList';
-import SearchBar from '../SearchBar/SearchBar';
-import Yelp from '../../util/Yelp';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
 
 class App extends React.Component {
   constructor(props) {
